9d1c21cadefe72cb9a295a4107fe94ed2c96d21f
Jimin Huh <jiminhuh95@gmail.com>
2020-01-16 22:00:09 -0600

    storyboard label and image view

---
+++ iAmRichApp/iAmRichApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iAmRichApp
+//
+//  Created by Jimin Huh on 1/16/20.
+//  Copyright © 2020 Jimin Huh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
